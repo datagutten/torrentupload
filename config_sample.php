4467fde22291994dba66a604624cf86fbb02b20a
datagutten <datagutten@datagutten.net>
2013-08-13 21:20:52 +0200

    Added sample config file

---
+++ config_sample.php
@@ -0,0 +1,21 @@
+<?Php
+//Login
+$site['username'] = "user";
+$site['password'] = "pass";
+$site['url']= "http://site.com";
+//Where the torrent get created
+$torrent_file_dir = "/home/torrent/tmp";
+$torrent_auto_dir="/mnt/torrent/torrent";
+
+$snapshots=true;
+$imgur_key = "";
+$api_key = "";
+$api_secret = "";
+
+$tvdb_key='';
+
+$ftp_host='';
+$ftp_user='';
+$ftp_password='';
+
+?>
